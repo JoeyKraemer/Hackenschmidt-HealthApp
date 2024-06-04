04016fee66945e8587a72c169c8d474765a4ad62
Bohdan Zakusylo <bohdan.zakusylo@student.nhlstenden.com>
2024-06-04 02:01:21 +0200

    finished 4 screens

---
+++ Hackenschmidt/SplashScreens/ProcessThreeGroup/ButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  ButtonView.swift
+//  Hackenschmidt
+//
+//  Created by Богдан Закусило on 04.06.2024.
+//
+
+import SwiftUI
+
+struct ButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ButtonView()
+}
---
+++ Hackenschmidt/SplashScreens/ProcessThreeGroup/ProcessThreeLogic.swift
@@ -0,0 +1,8 @@
+//
+//  ProcessThreeLogic.swift
+//  Hackenschmidt
+//
+//  Created by Богдан Закусило on 04.06.2024.
+//
+
+import Foundation
