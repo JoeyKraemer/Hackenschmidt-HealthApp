8e3162e2320a3856b28c80fdf7add49c3a622e22
Joey Krämer <joey.kramer@student.nhlstenden.com>
2024-06-10 15:20:28 +0200

    update supabase into its own file

--- Hackenschmidt/HackenschmidtApp.swift
+++ Hackenschmidt/HackenschmidtApp.swift
@@ -12,9 +12,6 @@ import SwiftUI
 struct HackenschmidtApp: App {
     let persistenceController = PersistenceController.shared
 
-    // supabase connection test
-    let client = SupabaseClient(supabaseURL: URL(string: "https://ggelsjpzpdreuoxgaxid.supabase.co")!, supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdnZWxzanB6cGRyZXVveGdheGlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ3MjgxMzcsImV4cCI6MjAzMDMwNDEzN30.X4BYTMMVwqSCExMOb-7UeqDciaQBmZxNfhiInR-S178")
-
     @State private var showSecondSplash = false
 
     var body: some Scene {
---
+++ Hackenschmidt/supabase/Supabase.swift
@@ -0,0 +1,13 @@
+//
+//  Supabase.swift
+//  Hackenschmidt
+//
+//  Created by Joey Krämer on 10.06.24.
+//
+
+import Supabase
+import Foundation
+
+// supabase connection test
+let client = SupabaseClient(supabaseURL: URL(string: "https://ggelsjpzpdreuoxgaxid.supabase.co")!, supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdnZWxzanB6cGRyZXVveGdheGlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ3MjgxMzcsImV4cCI6MjAzMDMwNDEzN30.X4BYTMMVwqSCExMOb-7UeqDciaQBmZxNfhiInR-S178")
+
