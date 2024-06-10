f57aa4e0a9dfeb1b36450817e0809b403d544f98
swiftformat <github-actions>
2024-06-10 13:20:59 +0000

    Autoformat swift files by swiftformat

--- Hackenschmidt/supabase/Supabase.swift
+++ Hackenschmidt/supabase/Supabase.swift
@@ -5,9 +5,8 @@
 //  Created by Joey Krämer on 10.06.24.
 //
 
-import Supabase
 import Foundation
+import Supabase
 
 // supabase connection test
 let client = SupabaseClient(supabaseURL: URL(string: "https://ggelsjpzpdreuoxgaxid.supabase.co")!, supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdnZWxzanB6cGRyZXVveGdheGlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ3MjgxMzcsImV4cCI6MjAzMDMwNDEzN30.X4BYTMMVwqSCExMOb-7UeqDciaQBmZxNfhiInR-S178")
-
