f75d46a8427c11a8b9ecea436bfc76fa557b9ca5
Joey Krämer <joey.kramer@student.nhlstenden.com>
2024-06-17 16:56:31 +0200

    add health kit support

--- Hackenschmidt/profile/EditProfileView.swift
+++ Hackenschmidt/profile/EditProfileView.swift
@@ -5,6 +5,7 @@
 //  Created by Joey Krämer on 17.06.24.
 //
 import SwiftUI
+import HealthKit
 
 struct EditProfileView: View {
     @Binding var name: String
@@ -13,6 +14,8 @@ struct EditProfileView: View {
     @Binding var sex: String
     @Binding var caloriesIntakeGoal: String
     @Binding var activityLevel: String
+    
+    let healthStore: HKHealthStore
     
     var body: some View {
         NavigationView {
@@ -46,14 +49,35 @@ struct EditProfileView: View {
             }
             .navigationBarTitle("Edit Profile", displayMode: .inline)
             .navigationBarItems(trailing: Button("Save") {
-                // Handle the save action here
+                saveToHealthKit()
             })
         }
     }
-}
-
-struct EditProfileView_Previews: PreviewProvider {
-    static var previews: some View {
-        EditProfileView(name: .constant("Jane"), weight: .constant("60 kg"), height: .constant("175 cm"), sex: .constant("Female"), caloriesIntakeGoal: .constant("2,000 cal"), activityLevel: .constant("Active"))
+    
+    private func saveToHealthKit() {
+        guard let weightValue = Double(weight.replacingOccurrences(of: " kg", with: "")),
+              let heightValue = Double(height.replacingOccurrences(of: " cm", with: "")),
+              let caloriesValue = Double(caloriesIntakeGoal.replacingOccurrences(of: " cal", with: "")) else {
+            return
+        }
+        
+        let weightType = HKQuantityType.quantityType(forIdentifier: .bodyMass)!
+        let heightType = HKQuantityType.quantityType(forIdentifier: .height)!
+        let caloriesType = HKQuantityType.quantityType(forIdentifier: .dietaryEnergyConsumed)!
+        
+        let weightQuantity = HKQuantity(unit: HKUnit.gramUnit(with: .kilo), doubleValue: weightValue)
+        let heightQuantity = HKQuantity(unit: HKUnit.meterUnit(with: .centi), doubleValue: heightValue)
+        let caloriesQuantity = HKQuantity(unit: HKUnit.kilocalorie(), doubleValue: caloriesValue)
+        
+        let weightSample = HKQuantitySample(type: weightType, quantity: weightQuantity, start: Date(), end: Date())
+        let heightSample = HKQuantitySample(type: heightType, quantity: heightQuantity, start: Date(), end: Date())
+        let caloriesSample = HKQuantitySample(type: caloriesType, quantity: caloriesQuantity, start: Date(), end: Date())
+        
+        healthStore.save([weightSample, heightSample, caloriesSample]) { success, error in
+            if !success {
+                // Handle the error
+                print("Failed to save HealthKit data: \(String(describing: error))")
+            }
+        }
     }
 }
--- Hackenschmidt/profile/ProfileView.swift
+++ Hackenschmidt/profile/ProfileView.swift
@@ -5,6 +5,7 @@
 //  Created by Joey Krämer on 17.06.24.
 //
 import SwiftUI
+import HealthKit
 
 struct ProfileView: View {
     @State private var isEditViewPresented = false
@@ -14,6 +15,8 @@ struct ProfileView: View {
     @State private var sex: String = "Female"
     @State private var caloriesIntakeGoal: String = "2,000 cal"
     @State private var activityLevel: String = "Active"
+    
+    private let healthStore = HKHealthStore()
     
     var body: some View {
         VStack(spacing: 20) {
@@ -80,7 +83,32 @@ struct ProfileView: View {
         }
         .padding()
         .sheet(isPresented: $isEditViewPresented) {
-            EditProfileView(name: $name, weight: $weight, height: $height, sex: $sex, caloriesIntakeGoal: $caloriesIntakeGoal, activityLevel: $activityLevel)
+            EditProfileView(name: $name, weight: $weight, height: $height, sex: $sex, caloriesIntakeGoal: $caloriesIntakeGoal, activityLevel: $activityLevel, healthStore: healthStore)
+        }
+        .onAppear {
+            requestHealthKitAuthorization()
+        }
+    }
+    
+    private func requestHealthKitAuthorization() {
+        let healthKitTypesToRead: Set = [
+            HKObjectType.quantityType(forIdentifier: .bodyMass)!,
+            HKObjectType.quantityType(forIdentifier: .height)!,
+            HKObjectType.characteristicType(forIdentifier: .biologicalSex)!,
+            HKObjectType.quantityType(forIdentifier: .dietaryEnergyConsumed)!
+        ]
+        
+        let healthKitTypesToWrite: Set = [
+            HKObjectType.quantityType(forIdentifier: .bodyMass)!,
+            HKObjectType.quantityType(forIdentifier: .height)!,
+            HKObjectType.quantityType(forIdentifier: .dietaryEnergyConsumed)!
+        ]
+        
+        healthStore.requestAuthorization(toShare: healthKitTypesToWrite, read: healthKitTypesToRead) { success, error in
+            if !success {
+                // Handle the error
+                print("HealthKit authorization failed: \(String(describing: error))")
+            }
         }
     }
 }
