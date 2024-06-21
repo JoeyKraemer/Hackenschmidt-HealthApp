d00791180a7470ed457426ef61006290879a6c1c
Joey Krämer <joey.kramer@student.nhlstenden.com>
2024-06-21 07:07:08 +0200

    Added workout view on homescreen

--- Hackenschmidt/HomepageGroup/Homepage.swift
+++ Hackenschmidt/HomepageGroup/Homepage.swift
@@ -41,10 +41,23 @@ struct Homepage: View {
                             Spacer()
                         }
                         .padding(.horizontal)
-
+                        WorkoutChartView(workoutData: [
+                            WorkoutData(day: "Day 9", count: 1),
+                            WorkoutData(day: "Day 10", count: 2),
+                            WorkoutData(day: "Day 11", count: 3),
+                            WorkoutData(day: "Day 12", count: 4),
+                            WorkoutData(day: "Day 13", count: 2),
+                            WorkoutData(day: "Day 14", count: 1),
+                            WorkoutData(day: "Day 15", count: 3),
+                            WorkoutData(day: "Day 16", count: 0),
+                            WorkoutData(day: "Day 17", count: 0),
+                            WorkoutData(day: "Day 18", count: 3),
+                            WorkoutData(day: "Day 19", count: 3),
+                            WorkoutData(day: "Day 20", count: 4),
+                        ])
                         Spacer()
                     }
-                    .frame(height: 200)
+                    .frame(height: 300)
                     .background(Color.white)
                     .cornerRadius(15)
                     .shadow(radius: 5)
